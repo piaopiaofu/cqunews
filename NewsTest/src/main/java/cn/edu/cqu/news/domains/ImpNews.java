ae00b2d19b81b4e4db7411795f10b1cbffd8e6f3
PiaopiaoFu <1054035336@qq.com>
2015-05-05 21:01:22 +0800

    实现信息抓取并分页显示

---
+++ NewsTest/src/main/java/cn/edu/cqu/news/domains/ImpNews.java
@@ -0,0 +1,38 @@
+package cn.edu.cqu.news.domains;
+
+public class ImpNews {
+	private String id;
+	private String title;
+	private String content;
+	
+	public ImpNews(String id,String title,String content) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.content = content;
+	}
+
+	public ImpNews() {
+		super();
+	}
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
